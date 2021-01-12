b5178495753d5551edd5d4d251b58e0e7335c610
sandy_chang <sandy230207@gmail.com>
2021-01-12 21:21:46 +0800

    Update ns3.sh

--- ns3.sh
+++ ns3.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-sudo apt update -y
-sudo apt-get -y install gcc g++ python python-dev
-sudo apt-get -y install mercurial
-sudo apt-get -y install bzr
-sudo apt-get -y install qt4-dev-tools libqt4-dev
-sudo apt-get -y install cmake libc6-dev libc6-dev-i386
-sudo apt-get -y install gdb valgrind
 sudo apt-get -y install gsl-bin libgsl2
 sudo apt-get -y install flex bison libfl-dev
 sudo apt-get -y install g++-3.4 gcc-3.4
