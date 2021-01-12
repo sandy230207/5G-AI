199417c184c6b6d015501dfe1d9c47d2f4a75899
sandy_chang <sandy230207@gmail.com>
2021-01-12 21:19:34 +0800

    Update ns3.sh

--- ns3.sh
+++ ns3.sh
@@ -1,22 +1,23 @@
-apt update -y
-apt-get -y install gcc g++ python python-dev
-apt-get -y install mercurial
-apt-get -y install bzr
-apt-get -y install qt4-dev-tools libqt4-dev
-apt-get -y install cmake libc6-dev libc6-dev-i386
-apt-get -y install gdb valgrind
-apt-get -y install gsl-bin libgsl2
-apt-get -y install flex bison libfl-dev
-apt-get -y install g++-3.4 gcc-3.4
-apt-get -y install tcpdump
-apt-get -y install sqlite sqlite3 libsqlite3-dev
-apt-get -y install libxml2 libxml2-dev
-apt-get -y install libgtk2.0-0 libgtk2.0-dev
-apt-get -y install vtun lxc
-apt-get -y install uncrustify
-apt-get -y install doxygen graphviz imagemagick
-apt-get -y install texlive texlive-extra-utils texlive-latex-extra
-apt-get -y install python-sphinx dia
-apt-get -y install python-pygraphviz python-kiwi python-pygoocanvas libgoocanvas-dev
-apt-get -y install libboost-signals-dev libboost-filesystem-dev
-apt-get -y install openmpi-bin openmpi-common openmpi-doc libopenmpi-dev
+#!/bin/bash
+sudo apt update -y
+sudo apt-get -y install gcc g++ python python-dev
+sudo apt-get -y install mercurial
+sudo apt-get -y install bzr
+sudo apt-get -y install qt4-dev-tools libqt4-dev
+sudo apt-get -y install cmake libc6-dev libc6-dev-i386
+sudo apt-get -y install gdb valgrind
+sudo apt-get -y install gsl-bin libgsl2
+sudo apt-get -y install flex bison libfl-dev
+sudo apt-get -y install g++-3.4 gcc-3.4
+sudo apt-get -y install tcpdump
+sudo apt-get -y install sqlite sqlite3 libsqlite3-dev
+sudo apt-get -y install libxml2 libxml2-dev
+sudo apt-get -y install libgtk2.0-0 libgtk2.0-dev
+sudo apt-get -y install vtun lxc
+sudo apt-get -y install uncrustify
+sudo apt-get -y install doxygen graphviz imagemagick
+sudo apt-get -y install texlive texlive-extra-utils texlive-latex-extra
+sudo apt-get -y install python-sphinx dia
+sudo apt-get -y install python-pygraphviz python-kiwi python-pygoocanvas libgoocanvas-dev
+sudo apt-get -y install libboost-signals-dev libboost-filesystem-dev
+sudo apt-get -y install openmpi-bin openmpi-common openmpi-doc libopenmpi-dev
